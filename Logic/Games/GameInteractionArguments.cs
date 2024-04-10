55115d389c708f6c9addf15db088cebcacc56e0b
Benedikt Müll <benedikt.muell@zimmer-group.com>
2024-04-10 11:04:07 +0200

    added arguments for game interactions

---
+++ Logic/Games/GameInteractionArguments.cs
@@ -0,0 +1,26 @@
+﻿using Logic.Communications.Actions;
+using Logic.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Games
+{
+    public class GameInteractionArguments<T> where T : Communications.Actions.Action
+    {
+        public PieceColor? actor;
+        public T action;
+        public bool handled;
+
+        public GameInteractionArguments(T action) : this(null, action) { }
+
+        public GameInteractionArguments(PieceColor? actor, T action)
+        {
+            this.actor = actor;
+            this.action = action;
+            handled = false;
+        }
+    }
+}
