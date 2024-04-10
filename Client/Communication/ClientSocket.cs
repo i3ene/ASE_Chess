9f9e8f7010934fdb4958f11fd7c71aa2a4dfec9f
Benedikt Müll <benedikt.muell@zimmer-group.com>
2024-04-10 09:15:29 +0200

    implemented generic socket client

---
+++ Client/Communication/ClientSocket.cs
@@ -0,0 +1,23 @@
+﻿using Logic.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Communication
+{
+    public class ClientSocket<T> : Socket<T>
+    {
+        public readonly CancellationToken token = new CancellationToken();
+
+        public ClientSocket() : base(new ClientWebSocket()) { }
+
+        public async Task ConnectAsync(Uri uri)
+        {
+            await ((ClientWebSocket)_socket).ConnectAsync(uri, token);
+            await Listen();
+        }
+    }
+}
