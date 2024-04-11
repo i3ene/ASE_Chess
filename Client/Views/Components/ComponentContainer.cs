0986e7f5d5f02d60bb3082826da46dd04dac347e
Benedikt Müll <benedikt.muell@zimmer-group.com>
2024-04-11 08:29:04 +0200

    implemented child logic on component container

--- Client/Views/Components/ComponentContainer.cs
+++ Client/Views/Components/ComponentContainer.cs
@@ -14,5 +14,22 @@ namespace Client.Views.Components
         {
             childs = new List<Component>();
         }
+
+        public void AddChild(Component child)
+        {
+            child.parent = this;
+            child.OnUpdate += Update;
+            childs.Add(child);
+            Update();
+        }
+
+        public void RemoveChild(Component child)
+        {
+            bool success = childs.Remove(child);
+            if (!success) return;
+            child.parent = null;
+            child.OnUpdate -= Update;
+            Update();
+        }
     }
 }
