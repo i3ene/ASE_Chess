a25dd92112997579366635e9d059bf96cf0e2f15
Benedikt Müll <benedikt.muell@zimmer-group.com>
2024-04-15 10:09:19 +0200

    added component dimension

---
+++ Client/Views/Components/Styles/ComponentDimension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Views.Components.Styles
+{
+    public class ComponentDimension
+    {
+        public readonly ComponentPosition position;
+        public readonly ComponentSize innerSize;
+        public readonly ComponentSize outerSize;
+
+        public ComponentDimension() : this(new ComponentPosition(), new ComponentSize(), new ComponentSize()) { }
+
+        public ComponentDimension(ComponentPosition position, ComponentSize innerSize, ComponentSize outerSize)
+        {
+            this.innerSize = innerSize;
+            this.outerSize = outerSize;
+            this.position = position;
+        }
+    }
+}
