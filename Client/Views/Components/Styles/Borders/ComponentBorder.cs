5d855ad59883b71c2a1f3919c14c14d84e599282
Benedikt Müll <benedikt.muell@zimmer-group.com>
2024-04-15 10:09:05 +0200

    implemented component border

--- Client/Views/Components/Styles/Borders/ComponentBorder.cs
+++ Client/Views/Components/Styles/Borders/ComponentBorder.cs
@@ -8,5 +8,24 @@ namespace Client.Views.Components.Styles.Borders
 {
     public class ComponentBorder
     {
+        public ComponentBorderStyle style;
+        public ComponentBorderPosition[] positions;
+        public bool isEnabled { get => style != ComponentBorderStyle.None; }
+
+        public ComponentBorder()
+        {
+            style = ComponentBorderStyle.None;
+            positions = GetAllPositions();
+        }
+
+        public bool HasPosition(ComponentBorderPosition position)
+        {
+            return positions.Contains(position);
+        }
+
+        private static ComponentBorderPosition[] GetAllPositions()
+        {
+            return Enum.GetValues(typeof(ComponentBorderPosition)).Cast<ComponentBorderPosition>().ToArray();
+        }
     }
 }
