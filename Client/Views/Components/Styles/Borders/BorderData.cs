d35c44ce797c199f9a9db5e7197304c92a0441e1
Benedikt Müll <benedikt.muell@zimmer-group.com>
2024-04-19 16:27:12 +0200

    added border display data

---
+++ Client/Views/Components/Styles/Borders/BorderData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Views.Components.Styles.Borders
+{
+    public static class BorderData
+    {
+        public readonly static char[][] symbols = [
+            [
+                ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+            ],
+            [
+                '\u2554', '\u2557', '\u255a', '\u255d', '\u2550', '\u2550', '\u2551', '\u2551'
+            ],
+            [
+                '\u250c', '\u2510', '\u2514', '\u2518', '\u2500', '\u2500', '\u2502', '\u2502'
+            ],
+            [
+                '\u250f', '\u2513', '\u2517', '\u251b', '\u2501', '\u2501', '\u2503', '\u2503'
+            ],
+            [
+                '\u256d', '\u256e', '\u2570', '\u256f', '\u2500', '\u2500', '\u2502', '\u2502'
+            ],
+            [
+                ' ', ' ', ' ', ' ', '\u2500', '\u2500', '\u2502', '\u2502'
+            ],
+            [
+                '\u253c', '\u253c', '\u253c', '\u253c', '\u2500', '\u2500', '\u2502', '\u2502'
+            ],
+            [
+                '\u253c', '\u253c', '\u253c', '\u253c', '\u252c', '\u2534', '\u2524', '\u251c'
+            ],
+            [
+                '\u250c', '\u2510', '\u2514', '\u2518', '\u254c', '\u254c', '\u2506', '\u2506'
+            ],
+        ];
+    }
+}
