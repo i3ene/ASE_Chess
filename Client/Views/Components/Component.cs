a1593707ec8a80ec3dd83bbcaeb5601016840e09
I3ene <benedikt.muell@t-online.de>
2024-04-19 16:41:25 +0200

    added component view service

--- Client/Views/Components/Component.cs
+++ Client/Views/Components/Component.cs
@@ -19,7 +19,7 @@ namespace Client.Views.Components
         public readonly ComponentSize size;
         public readonly ComponentBorder border;
         public readonly ComponentAlignment alignment;
-        public readonly ComponentService service;
+        public readonly ComponentViewService viewService;
 
         public Component()
         {
@@ -27,7 +27,7 @@ namespace Client.Views.Components
             size = new ComponentSize();
             border = new ComponentBorder();
             alignment = new ComponentAlignment();
-            service = new ComponentService();
+            viewService = new ComponentViewService(new ComponentService());
         }
 
         public void Update()
@@ -35,81 +35,15 @@ namespace Client.Views.Components
             OnUpdate?.Invoke();
         }
 
-        public abstract ContentString[] GetView();
+        protected abstract ContentString[] GetView();
 
         public ContentString[] View()
         {
             ContentString[] view = GetView();
-            view = ConstrainView(view);
-            view = AddBorder(view);
+            view = viewService.ConstrainView(this, view);
+            view = viewService.AddBorder(this, view);
             return view;
-        }
-
-        private ContentString[] ConstrainView(ContentString[] view)
-        {
-            List<ContentString> constrainedView = new List<ContentString>();
-            int innerWidth = service.CalculateInnerWidth(this);
-            int innerHeight = service.CalculateInnerHeight(this);
-            for (int i = 0; i < innerHeight; i++)
-            {
-                ContentString viewLine = view[i].Substring(0, innerWidth);
-                constrainedView[i] = viewLine;
-            }
-            return constrainedView.ToArray();
-        }
-
-        private ContentString[] AddBorder(ContentString[] view)
-        {
-            if (border.isEnabled) return view;
-
-            List<ContentString> viewWithBorder = new List<ContentString>(view);
-            int innerWidth = service.CalculateInnerWidth(this);
-
-            if (border.HasAnyTop())
-            {
-                viewWithBorder.Insert(0, new ContentString(new string(border.GetPosition(ComponentBorderPosition.Top), innerWidth)));
-            }
-            if (border.HasAnyBottom())
-            {
-                viewWithBorder.Add(new ContentString(new string(border.GetPosition(ComponentBorderPosition.Bottom), innerWidth)));
-            }
-            if (border.HasAnyLeft())
-            {
-                foreach (ContentString viewLine in viewWithBorder)
-                {
-                    viewLine.Insert(0, border.GetPosition(ComponentBorderPosition.Left).ToString());
-                }
-            }
-            if (border.HasAnyRight())
-            {
-                foreach (ContentString viewLine in viewWithBorder)
-                {
-                    viewLine.Add(border.GetPosition(ComponentBorderPosition.Right).ToString());
-                }
-            }
-
-            if (border.HasPosition(ComponentBorderPosition.TopLeft))
-            {
-                viewWithBorder[0].Inplace(0, border.GetPosition(ComponentBorderPosition.TopLeft).ToString());
-            }
-
-            if (border.HasPosition(ComponentBorderPosition.TopRight))
-            {
-                viewWithBorder[0].Inplace(viewWithBorder[0].Length - 1, border.GetPosition(ComponentBorderPosition.TopRight).ToString());
-            }
-
-            if (border.HasPosition(ComponentBorderPosition.BottomLeft))
-            {
-                viewWithBorder[viewWithBorder.Count - 1].Inplace(0, border.GetPosition(ComponentBorderPosition.BottomLeft).ToString());
-            }
-
-            if (border.HasPosition(ComponentBorderPosition.BottomRight))
-            {
-                viewWithBorder[viewWithBorder.Count - 1].Inplace(viewWithBorder[0].Length - 1, border.GetPosition(ComponentBorderPosition.BottomRight).ToString());
-            }
-
-            return viewWithBorder.ToArray();
-        }
+        }        
         
     }
 }
---
+++ Client/Views/Components/ComponentViewService.cs
@@ -0,0 +1,86 @@
+﻿using Client.Views.Components.Styles.Borders;
+using Client.Views.Contents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Views.Components
+{
+    public class ComponentViewService
+    {
+        private readonly ComponentService service;
+
+        public ComponentViewService(ComponentService service)
+        {
+            this.service = service;
+        }
+
+        public ContentString[] ConstrainView(Component component, ContentString[] view)
+        {
+            List<ContentString> constrainedView = new List<ContentString>();
+            int innerWidth = service.CalculateInnerWidth(component);
+            int innerHeight = service.CalculateInnerHeight(component);
+            for (int i = 0; i < innerHeight; i++)
+            {
+                ContentString viewLine = view[i].Substring(0, innerWidth);
+                constrainedView[i] = viewLine;
+            }
+            return constrainedView.ToArray();
+        }
+
+        public ContentString[] AddBorder(Component component, ContentString[] view)
+        {
+            if (component.border.isEnabled) return view;
+
+            List<ContentString> viewWithBorder = new List<ContentString>(view);
+            int innerWidth = service.CalculateInnerWidth(component);
+
+            if (component.border.HasAnyTop())
+            {
+                viewWithBorder.Insert(0, new ContentString(new string(component.border.GetPosition(ComponentBorderPosition.Top), innerWidth)));
+            }
+            if (component.border.HasAnyBottom())
+            {
+                viewWithBorder.Add(new ContentString(new string(component.border.GetPosition(ComponentBorderPosition.Bottom), innerWidth)));
+            }
+            if (component.border.HasAnyLeft())
+            {
+                foreach (ContentString viewLine in viewWithBorder)
+                {
+                    viewLine.Insert(0, component.border.GetPosition(ComponentBorderPosition.Left).ToString());
+                }
+            }
+            if (component.border.HasAnyRight())
+            {
+                foreach (ContentString viewLine in viewWithBorder)
+                {
+                    viewLine.Add(component.border.GetPosition(ComponentBorderPosition.Right).ToString());
+                }
+            }
+
+            if (component.border.HasPosition(ComponentBorderPosition.TopLeft))
+            {
+                viewWithBorder[0].Inplace(0, component.border.GetPosition(ComponentBorderPosition.TopLeft).ToString());
+            }
+
+            if (component.border.HasPosition(ComponentBorderPosition.TopRight))
+            {
+                viewWithBorder[0].Inplace(viewWithBorder[0].Length - 1, component.border.GetPosition(ComponentBorderPosition.TopRight).ToString());
+            }
+
+            if (component.border.HasPosition(ComponentBorderPosition.BottomLeft))
+            {
+                viewWithBorder[viewWithBorder.Count - 1].Inplace(0, component.border.GetPosition(ComponentBorderPosition.BottomLeft).ToString());
+            }
+
+            if (component.border.HasPosition(ComponentBorderPosition.BottomRight))
+            {
+                viewWithBorder[viewWithBorder.Count - 1].Inplace(viewWithBorder[0].Length - 1, component.border.GetPosition(ComponentBorderPosition.BottomRight).ToString());
+            }
+
+            return viewWithBorder.ToArray();
+        }
+    }
+}
