3a53f9606a9fdaf3b272b859e517048e3f194c10
I3ene <benedikt.muell@t-online.de>
2024-04-24 09:13:06 +0200

    changed input handling

--- Client/Views/Components/InputComponent.cs
+++ Client/Views/Components/InputComponent.cs
@@ -16,20 +16,20 @@ namespace Client.Views.Components
         {
             if (args.key.Key == ConsoleKey.Enter) return;
 
-            if (char.IsLetterOrDigit(args.key.KeyChar) || char.IsWhiteSpace(args.key.KeyChar))
-            {
-                ContentString text = GetText();
-                text.Add(args.key.KeyChar.ToString());
-                SetText(text);
-                args.handled = true;
-                return;
-            }
-
             if (args.key.Key == ConsoleKey.Backspace)
             {
                 ContentString text = GetText();
                 int length = Math.Max(text.Length - 2, -1);
                 text = text.Substring(0, length);
+                SetText(text);
+                args.handled = true;
+                return;
+            }
+
+            if (char.IsAscii(args.key.KeyChar) && !char.IsControl(args.key.KeyChar))
+            {
+                ContentString text = GetText();
+                text.Add(args.key.KeyChar.ToString());
                 SetText(text);
                 args.handled = true;
                 return;
