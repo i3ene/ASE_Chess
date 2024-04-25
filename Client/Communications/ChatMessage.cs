ca12796a5c27cdc937d9775af16fe31f164a1a52
I3ene <benedikt.muell@t-online.de>
2024-04-25 16:46:04 +0200

    added chat messages and repository

---
+++ Client/Communications/ChatMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Communications
+{
+    public class ChatMessage
+    {
+        public readonly string sender;
+        public readonly string message;
+
+        public ChatMessage(string message) : this(string.Empty, message) { }
+
+        public ChatMessage(string sender, string message)
+        {
+            this.sender = sender;
+            this.message = message;
+        }
+    }
+}
---
+++ Client/Communications/ChatRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Communications
+{
+    public class ChatRepository
+    {
+        private readonly List<ChatMessage> messages;
+
+        public delegate void CollectionChanged();
+        public event CollectionChanged? OnChange;
+
+        public ChatRepository()
+        {
+            messages = new List<ChatMessage>();
+        }
+
+        public void AddMessage(ChatMessage message)
+        {
+            messages.Add(message);
+            OnChange?.Invoke();
+        }
+
+        public ChatMessage[] GetAllMessages()
+        {
+            return messages.ToArray();
+        }
+    }
+}
