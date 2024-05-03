3032d935135ccdeac32aebab257cb2f947ef461a
I3ene <benedikt.muell@t-online.de>
2024-05-03 14:04:23 +0200

    added game tests

---
+++ Test/Logic/GameTest.cs
@@ -0,0 +1,40 @@
+﻿using Logic;
+using Logic.Boards;
+using Logic.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Logic
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestChangeTurn()
+        {
+            Game game = new Game();
+
+            PieceColor firstTurn = game.currentColor;
+            game.Turn();
+            PieceColor nextTurn = game.currentColor;
+
+            Assert.AreEqual(firstTurn, PieceColor.White);
+            Assert.AreEqual(nextTurn, PieceColor.Black);
+        }
+
+        [TestMethod]
+        public void TestFirstMove()
+        {
+            Game game = new Game();
+            BoardPosition sourcePosition = new BoardPosition(0, 1);
+            BoardPosition targetPosition = new BoardPosition(0, 2);
+
+            bool successfull = game.Move(sourcePosition, targetPosition);
+
+            Assert.IsTrue(successfull);
+        }
+    }
+}
