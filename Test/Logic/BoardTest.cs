33d667896f2654b5d447cebb344d4e042e6d9708
I3ene <benedikt.muell@t-online.de>
2024-05-03 14:04:18 +0200

    added board tests

---
+++ Test/Logic/BoardTest.cs
@@ -0,0 +1,45 @@
+﻿using Logic.Boards;
+using Logic.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Mocks;
+
+namespace Test.Logic
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void TestPiecePositionChange()
+        {
+            BoardMockup board = new BoardMockup();
+            BoardService service = new BoardService(board);
+            Piece piece = new Piece(PieceColor.White, PieceType.King, new BoardPosition());
+            BoardPosition target = new BoardPosition(1, 1);
+
+            service.Move(piece, target);
+
+            Assert.AreEqual(piece.position, target);
+        }
+
+        [TestMethod]
+        public void TestPawnStartMove()
+        {
+            Board board = new Board();
+            BoardService boardService = new BoardService(board);
+            PieceService pieceService = new PieceService();
+            PieceFactory pieceFactory = new PieceFactory(pieceService);
+            List<Piece> pieces = pieceFactory.CreatePieces(PieceColor.White, PieceType.Pawn);
+            board.AddPieces(pieces.ToArray());
+
+            Piece pawn = pieces[0];
+            BoardPosition targetPosition = pawn.position + new BoardPosition(0, 2);
+            bool successfull = boardService.Move(pawn, targetPosition);
+
+            Assert.IsTrue(successfull);
+        }
+    }
+}
